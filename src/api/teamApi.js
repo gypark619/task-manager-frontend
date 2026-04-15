6db35028f2dc43b3d200b85189bc83fed198d0d1
gypark <gypark619@gmail.com>
2026-04-15 15:37:45 +0900

    feat: Team API 추가 (등록/수정/삭제)

--- src/api/teamApi.js
+++ src/api/teamApi.js
@@ -3,3 +3,9 @@ import api from "./axios";
 export const getTeams = (params) => {
   return api.get("/teams", {params});
 };
+
+export const createTeam = (data) => api.post("/teams", data);
+
+export const updateTeam = (id, data) => api.put(`/teams/${id}`, data);
+
+export const deleteTeam = (id) => api.delete(`/teams/${id}`);
